4370911fa047a6aa471e9c1ac0cc63ad31fdf8f2
Alexandre <alexandre.balbino@gsupgroup.com>
2019-04-01 18:16:35 -0300

    Part 2 - Fisrt component

---
+++ alurapic/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { PhotosModule } from './photos/photos.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PhotosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
