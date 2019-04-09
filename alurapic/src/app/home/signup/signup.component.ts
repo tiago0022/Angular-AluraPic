02e755d4eb5f487dfb80f7c4faa0a4ef5a3b6f94
Tiago Araujo <tiago.araujo@gsupgroup.com>
2019-04-09 20:21:33 -0300

    2.4.6 (partial) Assyncronous validator

--- alurapic/src/app/home/signup/signup.component.ts
+++ alurapic/src/app/home/signup/signup.component.ts
@@ -1,5 +1,7 @@
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 
 @Component({
   selector: 'app-signup',
@@ -10,7 +12,9 @@ export class SignupComponent implements OnInit {
 
   signupForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private userNotTakenValidatorService: UserNotTakenValidatorService) { }
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
@@ -30,10 +34,11 @@ export class SignupComponent implements OnInit {
       userName: ['',
         [
           Validators.required,
-          Validators.pattern(/^[a-z0-9\-]/),
+          lowerCaseValidator,
           Validators.minLength(2),
           Validators.maxLength(30)
-        ]
+        ],
+        this.userNotTakenValidatorService.checkUserNameTaken()
       ],
       password: ['',
         [
---
+++ alurapic/src/app/shared/validators/lower-case.validator.ts
@@ -0,0 +1,10 @@
+import { Validators, AbstractControl } from '@angular/forms';
+
+export function lowerCaseValidator(control: AbstractControl) {
+
+    if (control.value.trim() && !/^[a-z0-9\-]/.test(control.value)) {
+        return { lowerCase: true };
+    }
+
+    return null;
+}
